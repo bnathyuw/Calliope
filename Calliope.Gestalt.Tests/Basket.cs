4661cdf45b21fc099b15c46d5a2166a282368951
matthew butt <matthew.butt@7digital.com>
2012-12-10 12:10:17 +0000

    Pull out basket

---
+++ Calliope.Gestalt.Tests/Basket.cs
@@ -0,0 +1,8 @@
+﻿namespace Calliope.Gestalt.Tests
+{
+	public class Basket
+	{
+		public int Id { get; set; }
+		public string[] Items { get; set; }
+	}
+}
--- Calliope.Gestalt.Tests/BasketTests.cs
+++ Calliope.Gestalt.Tests/BasketTests.cs
@@ -70,10 +70,4 @@ namespace Calliope.Gestalt.Tests
 
 		}
 	}
-
-	public class Basket
-	{
-		public int Id { get; set; }
-		public string[] Items { get; set; }
-	}
 }
