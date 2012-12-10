b6b2c9033887ca06dba3313206e2bca230ef7f0d
matthew butt <matthew.butt@7digital.com>
2012-12-10 11:13:15 +0000

    First passing feature test (Gestalt test still failing).

---
+++ Calliope.Basket.Feature.Tests/BasketModuleTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+using Nancy;
+using Nancy.Testing;
+
+namespace Calliope.Basket.Feature.Tests
+{
+	[TestFixture]
+	public class BasketModuleTests
+	{
+		[Test]
+		public void Post_basket_returns_basket()
+		{
+			Console.WriteLine("Testing {0}", typeof (BasketModule));
+			var browser = new Browser(new DefaultNancyBootstrapper());
+			var browserResponse = browser.Post("/", with =>
+				                                        {
+					                                        with.Body("");
+					                                        with.Accept("application/json");
+				                                        });
+			Assert.That(browserResponse != null, "browserResponse != null");
+			var basket = browserResponse.Body.DeserializeJson<Basket>();
+			Assert.That(basket != null, "basket != null");
+		}
+	}
+	public class Basket
+	{
+		public int Id { get; set; }
+		public string[] Items { get; set; }
+	}
+}
---
+++ Calliope.Basket/BasketModule.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+
+namespace Calliope.Basket
+{
+	public class BasketModule:NancyModule
+	{
+		public BasketModule()
+		{
+			Post["/"] = o => Negotiate.WithModel(new Basket {Id = 1, Items = new string[] {}});
+		}
+	}
+	public class Basket
+	{
+		public int Id { get; set; }
+		public string[] Items { get; set; }
+	}
+}
