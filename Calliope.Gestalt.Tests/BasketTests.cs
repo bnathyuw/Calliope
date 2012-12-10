e756f430a2b8681a0903c97b879217d60846661b
matthew butt <matthew.butt@7digital.com>
2012-12-10 12:28:46 +0000

    More refactoring

--- Calliope.Gestalt.Tests/BasketTests.cs
+++ Calliope.Gestalt.Tests/BasketTests.cs
@@ -5,22 +5,26 @@ namespace Calliope.Gestalt.Tests
 	[TestFixture]
 	public class BasketTests
 	{
+		private const string ApplicationRoot = "http://localhost/calliope";
+
 		[Test]
 		public void CreateBasket_creates_a_basket_which_can_be_retrieved()
 		{
-			var postBasketResponse = WebRequester.DoRequest("http://localhost/calliope/baskets/", "POST");
+			var postBasketResponse = WebRequester.DoRequest(ApplicationRoot + "/baskets/", "POST");
 
-			var postBasket = postBasketResponse.Deserialize<Basket>();
-			Assert.That(postBasket != null, "basket != null");
+			var postedBasket = postBasketResponse.Deserialize<Basket>();
+			Assert.That(postedBasket != null, "basket != null");
 
 			var basketUrl = postBasketResponse["Location"];
-			Assert.That(basketUrl, Is.EqualTo("/baskets/1"));
+			Assert.That(basketUrl, Is.EqualTo("/baskets/" + postedBasket.Id));
 
-			var getBasketResponse = WebRequester.DoRequest("http://localhost/calliope" + basketUrl, "GET");
+			var getBasketResponse = WebRequester.DoRequest(ApplicationRoot + basketUrl, "GET");
 			
-			var getBasket = getBasketResponse.Deserialize<Basket>();
-			Assert.That(getBasket != null, "basket != null");
+			var gotBasket = getBasketResponse.Deserialize<Basket>();
+			Assert.That(gotBasket != null, "basket != null");
 
+			Assert.That(gotBasket.Id, Is.EqualTo(postedBasket.Id), "basket.Id");
+			Assert.That(gotBasket.Items, Is.EqualTo(postedBasket.Items), "basket.Items");
 		}
 	}
 }
