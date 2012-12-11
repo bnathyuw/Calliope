c6fb3ae09aca37d03242fceb230de3761ee0af92
matthew butt <matthew.butt@7digital.com>
2012-12-11 15:16:15 +0000

    Jiggling

--- Calliope.Purchase/PurchaseModule.cs
+++ Calliope.Purchase/PurchaseModule.cs
@@ -10,20 +10,22 @@ namespace Calliope.Purchase
 		{
 			Post["/"] = o =>
 				            {
-					            var purchase = this.Bind<Purchase>();
-					            MakePurchase(purchase);
+					            var request = this.Bind<Purchase>();
+					            var purchase = MakePurchase(request);
 					            return Negotiate.WithStatusCode(HttpStatusCode.Created)
 					                            .WithModel(purchase);
 				            };
 		}
 
-		private static void MakePurchase(Purchase purchase)
+		private static Purchase MakePurchase(Purchase purchase)
 		{
 			var basket = BasketServiceWrapper.Get(purchase.BasketId);
+	
 			PaymentServiceWrapper.MakePayment(basket.Total, "basket:" + basket.Id, purchase.CardToken);
-			purchase.Total = basket.Total;
 
+			purchase.Total = basket.Total;
 			purchase.Status = "successful";
+			return purchase;
 		}
 	}
 }
