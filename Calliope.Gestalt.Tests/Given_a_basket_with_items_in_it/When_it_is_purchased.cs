5f507e0cd09a4247b52f1ac885cb2890e1b692d6
matthew butt <matthew.butt@7digital.com>
2012-12-11 11:35:15 +0000

    First couple of tests of POST to /purchases/

---
+++ Calliope.Gestalt.Tests/Given_a_basket_with_items_in_it/When_it_is_purchased.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using NUnit.Framework;
+
+namespace Calliope.Gestalt.Tests.Given_a_basket_with_items_in_it
+{
+	[TestFixture]
+	public class When_it_is_purchased
+	{
+		private TestWebResponse<Purchase> _postPurchaseResponse;
+		private Purchase _purchase;
+		private Basket _basket;
+		private const string ApplicationRoot = "http://localhost/calliope";
+
+		[TestFixtureSetUp]
+		public void TestFixtureSetUp()
+		{
+			var postBasketResponse = WebRequester.DoRequest<Basket>(ApplicationRoot + "/baskets/", "POST");
+			_basket = postBasketResponse.Body;
+			var basketUrl = postBasketResponse["Location"];
+
+			var poems = WebRequester.DoRequest<IEnumerable<Poem>>(ApplicationRoot + "/poems/", "GET").Body.ToArray();
+			
+			WebRequester.DoRequest(basketUrl + "items/", "POST", new Item { Id = poems[0].Id });
+			WebRequester.DoRequest(basketUrl + "items/", "POST", new Item { Id = poems[1].Id });
+			WebRequester.DoRequest(basketUrl + "items/", "POST", new Item { Id = poems[2].Id });
+
+			_postPurchaseResponse = WebRequester.DoRequest(ApplicationRoot + "/purchases/", "POST", new Purchase {BasketId = _basket.Id});
+			_purchase = _postPurchaseResponse.Body;
+		}
+
+		[Test]
+		public void Then_the_response_code_is_created()
+		{
+			Assert.That(_postPurchaseResponse.StatusCode, Is.EqualTo(HttpStatusCode.Created));
+		}
+
+		[Test]
+		public void Then_the_purchase_echoes_back_the_basket_id()
+		{
+			Assert.That(_purchase.BasketId, Is.EqualTo(_basket.Id));
+		}
+	}
+
+	public class Purchase
+	{
+		public int BasketId { get; set; }
+	}
+}
---
+++ Calliope.Purchase/PurchaseModule.cs
@@ -0,0 +1,23 @@
+﻿using Nancy;
+using Nancy.ModelBinding;
+
+namespace Calliope.Purchase
+{
+	public class PurchaseModule:NancyModule
+	{
+		public PurchaseModule()
+		{
+			Post["/"] = o =>
+				            {
+					            var purchase = this.Bind<Purchase>();
+					            return Negotiate.WithStatusCode(HttpStatusCode.Created)
+					                            .WithModel(purchase);
+				            };
+		}
+	}
+
+	public class Purchase
+	{
+		public int BasketId { get; set; }
+	}
+}
