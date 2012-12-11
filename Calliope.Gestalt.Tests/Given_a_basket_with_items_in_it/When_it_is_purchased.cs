6acfd149450de9656bf8865481a81b64ee3c40d5
matthew butt <matthew.butt@7digital.com>
2012-12-11 15:07:58 +0000

    Make a card transaction.

--- Calliope.Gestalt.Tests/Given_a_basket_with_items_in_it/When_it_is_purchased.cs
+++ Calliope.Gestalt.Tests/Given_a_basket_with_items_in_it/When_it_is_purchased.cs
@@ -12,6 +12,8 @@ namespace Calliope.Gestalt.Tests.Given_a_basket_with_items_in_it
 		private Purchase _purchase;
 		private Basket _basket;
 		private Poem[] _poems;
+		private CardTransaction _cardTransaction;
+		private int _amount;
 		private const string ApplicationRoot = "http://localhost/calliope";
 
 		[TestFixtureSetUp]
@@ -22,13 +24,18 @@ namespace Calliope.Gestalt.Tests.Given_a_basket_with_items_in_it
 			var basketUrl = postBasketResponse["Location"];
 
 			_poems = WebRequester.DoRequest<IEnumerable<Poem>>(ApplicationRoot + "/poems/", "GET").Body.ToArray();
-			
+			_amount = _poems.Take(3).Sum(p => p.Price);
+
 			WebRequester.DoRequest(basketUrl + "items/", "POST", new Item { Id = _poems[0].Id });
 			WebRequester.DoRequest(basketUrl + "items/", "POST", new Item { Id = _poems[1].Id });
 			WebRequester.DoRequest(basketUrl + "items/", "POST", new Item { Id = _poems[2].Id });
 
-			_postPurchaseResponse = WebRequester.DoRequest(ApplicationRoot + "/purchases/", "POST", new Purchase {BasketId = _basket.Id});
+			const string cardToken = "123456";
+			_postPurchaseResponse = WebRequester.DoRequest(ApplicationRoot + "/purchases/", "POST", new Purchase {BasketId = _basket.Id, CardToken = cardToken});
 			_purchase = _postPurchaseResponse.Body;
+
+			var cardTransactions = WebRequester.DoRequest<IEnumerable<CardTransaction>>(ApplicationRoot + "/stub/payment-provider/cards/" + cardToken + "/transactions/", "GET").Body;
+			_cardTransaction = cardTransactions.Last();
 		}
 
 		[Test]
@@ -46,7 +53,7 @@ namespace Calliope.Gestalt.Tests.Given_a_basket_with_items_in_it
 		[Test]
 		public void Then_the_purchase_shows_the_correct_total()
 		{
-			Assert.That(_purchase.Total, Is.EqualTo(_poems.Take(3).Sum(p => p.Price)));
+			Assert.That(_purchase.Total, Is.EqualTo(_amount));
 		}
 
 		[Test]
@@ -54,6 +61,25 @@ namespace Calliope.Gestalt.Tests.Given_a_basket_with_items_in_it
 		{
 			Assert.That(_purchase.Status, Is.EqualTo("successful"));
 		}
+
+		[Test]
+		public void Then_a_transaction_is_made_against_my_card()
+		{
+			Assert.That(_cardTransaction.Reference, Is.EqualTo("basket:" + _basket.Id));
+		}
+
+		[Test]
+		public void Then_the_transaction_amount_is_correct()
+		{
+			Assert.That(_cardTransaction.Amount, Is.EqualTo(_amount));
+		}
+	}
+
+	public class CardTransaction
+	{
+		public string Reference { get; set; }
+
+		public int Amount { get; set; }
 	}
 
 	public class Purchase
@@ -63,5 +89,7 @@ namespace Calliope.Gestalt.Tests.Given_a_basket_with_items_in_it
 		public int Total { get; set; }
 
 		public string Status { get; set; }
+
+		public string CardToken { get; set; }
 	}
 }
---
+++ Calliope.Stub.PaymentProvider/TransactionModule.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Nancy;
+using Nancy.ModelBinding;
+
+namespace Calliope.Stub.PaymentProvider
+{
+	public class TransactionModule:NancyModule
+	{
+		public TransactionModule()
+		{
+			Get["/cards/{cardtoken}/transactions/"] = o =>
+				                                          {
+					                                          var cardTransactions = TransactionStore.GetAll((string) o.cardtoken);
+					                                          return Negotiate.WithModel(cardTransactions);
+				                                          };
+
+			Post["/cards/{cardtoken}/transactions/"] = o =>
+				                                           {
+					                                           var cardTransaction = this.Bind<CardTransaction>();
+					                                           cardTransaction.CardToken = (string) o.cardtoken;
+					                                           TransactionStore.Add(cardTransaction);
+					                                           return Negotiate.WithStatusCode(HttpStatusCode.Created)
+					                                                           .WithModel(cardTransaction);
+				                                           };
+		} 
+	}
+
+	public static class TransactionStore
+	{
+		private static readonly IList<CardTransaction> Store = new List<CardTransaction>();
+
+		public static IEnumerable<CardTransaction> GetAll(string cardToken)
+		{
+			return Store.Where(ct => ct.CardToken == cardToken);
+		}
+
+		public static void Add(CardTransaction cardTransaction)
+		{
+			Store.Add(cardTransaction);
+		}
+	}
+
+	public class CardTransaction
+	{
+		public string CardToken { get; set; }
+		public int Amount { get; set; }
+		public string Reference { get; set; }
+	}
+
+}
